f93d143a349eb1ca84a7638dd6f11af98a4edcf6
Hammad Majid <hammadmajid@proton.me>
2025-08-25 05:18:56 +0500

    consume the new paddle and ball module to simplify code

--- main.go
+++ main.go
@@ -16,74 +16,34 @@ const (
 )
 
 type Game struct {
-	topPaddlePosX, topPaddlePosY    float32 // top paddle position
-	topPaddleWidth, topPaddleHeight float32 // top paddle size
-
-	btmPaddlePosX, btmPaddlePosY    float32 // bottom paddle position
-	btmPaddleWidth, btmPaddleHeight float32 // bottom paddle size
+	top    Paddle
+	bottom Paddle
 
 	paddleSpeed float32 // movement speed of both paddles
 
-	targetX, targetY   float32 // target position
-	targetVX, targetVY float32 // target velocity
-	targetSize         float32 // target size
+	ball Ball
 }
 
 func (g *Game) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyK) {
-		g.topPaddlePosX += g.paddleSpeed
-		g.btmPaddlePosX += -g.paddleSpeed
+		g.top.X += g.paddleSpeed
+		g.bottom.X -= g.paddleSpeed
 	} else if ebiten.IsKeyPressed(ebiten.KeyJ) {
-		g.topPaddlePosX += -g.paddleSpeed
-		g.btmPaddlePosX += g.paddleSpeed
+		g.top.X -= g.paddleSpeed
+		g.bottom.X += g.paddleSpeed
 	}
 
 	const padding = 16
 
-	// clamp top paddle
-	if g.topPaddlePosX < padding {
-		g.topPaddlePosX = padding
-	}
-	if g.topPaddlePosX+g.topPaddleWidth > float32(screenW-padding) {
-		g.topPaddlePosX = float32(screenW) - g.topPaddleWidth - padding
-	}
+	// clamp paddle movement
+	g.top.Clamp(screenW, padding)
+	g.bottom.Clamp(screenW, padding)
 
-	// clamp bottom paddle
-	if g.btmPaddlePosX < padding {
-		g.btmPaddlePosX = padding
-	}
-	if g.btmPaddlePosX+g.btmPaddleWidth > float32(screenW-padding) {
-		g.btmPaddlePosX = float32(screenW) - g.btmPaddleWidth - padding
-	}
-
-	g.targetX += g.targetVX
-	g.targetY += g.targetVY
-
-	// bounce left/right walls
-	if g.targetX < padding {
-		g.targetX = padding
-		g.targetVX = -g.targetVX
-	}
-	if g.targetX+g.targetSize > float32(screenW-padding) {
-		g.targetX = float32(screenW-padding) - g.targetSize
-		g.targetVX = -g.targetVX
-	}
-
-	// bounce top paddle
-	if g.targetY <= g.topPaddlePosY+g.topPaddleHeight &&
-		g.targetX+g.targetSize >= g.topPaddlePosX &&
-		g.targetX <= g.topPaddlePosX+g.topPaddleWidth {
-		g.targetY = g.topPaddlePosY + g.topPaddleHeight
-		g.targetVY = -g.targetVY
-	}
-
-	// bounce bottom paddle
-	if g.targetY+g.targetSize >= g.btmPaddlePosY &&
-		g.targetX+g.targetSize >= g.btmPaddlePosX &&
-		g.targetX <= g.btmPaddlePosX+g.btmPaddleWidth {
-		g.targetY = g.btmPaddlePosY - g.targetSize
-		g.targetVY = -g.targetVY
-	}
+	// ball movment and bounce
+	g.ball.Move()
+	g.ball.CheckWalls(screenW, screenH, 16)
+	g.ball.CheckPaddle(g.top, true)
+	g.ball.CheckPaddle(g.bottom, false)
 
 	return nil
 }
@@ -91,10 +51,10 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{20, 20, 20, 0})
 
-	vector.DrawFilledRect(screen, g.topPaddlePosX, g.topPaddlePosY, g.topPaddleWidth, g.topPaddleHeight, color.White, true)
-	vector.DrawFilledRect(screen, g.btmPaddlePosX, g.btmPaddlePosY, g.btmPaddleWidth, g.btmPaddleHeight, color.White, true)
+	vector.DrawFilledRect(screen, g.top.X, g.top.Y, g.top.W, g.top.H, color.White, true)
+	vector.DrawFilledRect(screen, g.bottom.X, g.bottom.Y, g.bottom.W, g.bottom.H, color.White, true)
 
-	vector.DrawFilledRect(screen, g.targetX, g.targetY, g.targetSize, g.targetSize, color.RGBA{200, 0, 0, 255}, true)
+	vector.DrawFilledRect(screen, g.ball.X, g.ball.Y, g.ball.Size, g.ball.Size, color.RGBA{200, 0, 0, 255}, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -105,23 +65,18 @@ func main() {
 	ebiten.SetWindowSize(screenW, screenH)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{
-		topPaddlePosX:   screenW / 2,
-		topPaddlePosY:   0.05 * float32(screenH),
-		topPaddleWidth:  paddleW,
-		topPaddleHeight: paddleH,
-		btmPaddlePosX:   screenW / 2,
-
-		btmPaddlePosY:   float32(screenH) - paddleH - (0.05 * float32(screenH)),
-		btmPaddleWidth:  paddleW,
-		btmPaddleHeight: paddleH,
+		top:    NewPaddle(screenW/2, 0.05*screenH),
+		bottom: NewPaddle(screenW/2, screenH-paddleH-(0.05*screenH)),
 
 		paddleSpeed: 3,
 
-		targetX:    float32(screenW/2 - 4),
-		targetY:    float32(screenH/2 - 4),
-		targetVX:   1.5,
-		targetVY:   2.5,
-		targetSize: 8,
+		ball: Ball{
+			X:    screenW/2 - 4,
+			Y:    screenH/2 - 4,
+			VX:   1.5,
+			VY:   2.5,
+			Size: 8,
+		},
 	}); err != nil {
 		log.Fatal(err)
 	}
