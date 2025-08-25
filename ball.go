c1bfca3ebe0f48be833b299c6574664fa2f2721d
Hammad Majid <hammadmajid@proton.me>
2025-08-25 05:38:07 +0500

    remove CheckWalls method

--- ball.go
+++ ball.go
@@ -14,17 +14,6 @@ func (b *Ball) Move() {
 func (b *Ball) BounceX() { b.VX = -b.VX }
 func (b *Ball) BounceY() { b.VY = -b.VY }
 
-func (b *Ball) CheckWalls(screenW, screenH, padding float32) {
-	if b.X < padding {
-		b.X = padding
-		b.BounceX()
-	}
-	if b.X+b.Size > screenW-padding {
-		b.X = screenW - padding - b.Size
-		b.BounceX()
-	}
-}
-
 func (b *Ball) CheckPaddle(p Paddle, fromTop bool) {
 	if p.Collides(b.X, b.Y, b.Size) {
 		if fromTop {
