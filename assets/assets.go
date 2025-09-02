ed811a9f14a7195249c2b9b499ccbd4d9dbbfcbf
Hammad Majid <hammadmajid@proton.me>
2025-09-03 01:29:42 +0500

    play background music in infinite loop

--- assets/assets.go
+++ assets/assets.go
@@ -1,10 +1,14 @@
 package assets
 
 import (
+	"bytes"
 	_ "embed"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
-	"log"
 )
 
 //go:embed fonts/array/Array-Bold.otf
@@ -12,7 +16,16 @@ var fontBytes []byte
 
 var Face font.Face
 
+//go:embed music/8_Bit_Surf_-_FesliyanStudios.com_-_David_Renda.mp3
+var bgmBytes []byte
+
+const sampleRate = 44100
+
+var AudioContext *audio.Context
+var BGMPlayer *audio.Player
+
 func init() {
+	// font
 	tt, err := opentype.Parse(fontBytes)
 	if err != nil {
 		log.Fatal(err)
@@ -25,4 +38,18 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// audio
+	AudioContext = audio.NewContext(sampleRate)
+	stream, err := mp3.DecodeWithSampleRate(sampleRate, bytes.NewReader(bgmBytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+	loop := audio.NewInfiniteLoop(stream, stream.Length())
+	BGMPlayer, err = AudioContext.NewPlayer(loop)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	BGMPlayer.Play()
 }
